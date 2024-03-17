311bf0c8ed8d515214f5523b1d3b90d9dc841396
SamuelMTL <95709293+SamuelMTL@users.noreply.github.com>
2024-03-17 20:20:40 -0300

    primeiro commit

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+
+var numA = 5;
+var numB = 3;
+
+var soma = numA + numB;
+var subtracao = numA - numB;
+var multiplicacao = numA * numB;
+var divisao = numA / numB;
+var resto = numA % numB;
+
+console.log("Resultado da soma entre " + numA + " e " + numB + ": " + soma);
+console.log("Resultado da subtração entre " + numA + " e " + numB + ": " + subtracao);
+console.log("Resultado da multiplicação entre " + numA + " e " + numB + ": " + multiplicacao);
+console.log("Resultado da divisão entre " + numA + " e " + numB + ": " + divisao);
+console.log("Resultado do resto da divisão entre " + numA + " e " + numB + ": " + resto);
